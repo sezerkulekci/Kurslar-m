3dd437469d7a0e9d7dc2338d4ca57b81874fe3bd
Sezer Kulekci <sezerklkci@gmail.com>
2024-01-31 11:55:41 +0300

    new cours project

---
+++ src/Course.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Course = ({ id, content, title, price, removeOneCourse }) => {
+  return (
+    <div className="card">
+      <div className="cardTitlePrice">
+        <h2 className="cardTitle">{title}</h2>
+        <h4 className="cardPrice">{price}TL</h4>
+      </div>
+      <p>{content}</p>
+      <button className="cardDeleteBtn" onClick={() => removeOneCourse(id)}>
+        Sil
+      </button>
+    </div>
+  );
+};
+
+export default Course;
